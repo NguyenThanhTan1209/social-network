ce7af2ddd83942dea85cc087be33f306cd8a515e
nguyenthanhtan12981 <nguyenthanhtan1298@gmail.com>
2025-05-20 16:43:32 +0700

    style: create HomeScreen UI

--- app/src/main/java/com/example/socialnetworkapp/view/HomeScreen.kt
+++ app/src/main/java/com/example/socialnetworkapp/view/HomeScreen.kt
@@ -1,16 +1,77 @@
 package com.example.socialnetworkapp.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Surface {
-        Column {
-            Text(text = "Home Screen")
-        }
+    val screenItems = listOf(
+        BottomNavItem("Newsfeed", Icons.Filled.Home),
+        BottomNavItem("Search", Icons.Filled.Search),
+        BottomNavItem("Notification", Icons.Filled.Notifications),
+        BottomNavItem("Profile", Icons.Filled.Person),
+    )
+    var currentIndex by remember { mutableStateOf(0) }
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+
+            ) {
+                screenItems.forEachIndexed { index, screenItems ->
+                    NavigationBarItem(
+                        icon = { Icon(screenItems.icon, contentDescription = screenItems.title) },
+                        label = { Text(screenItems.title) },
+                        selected = index == currentIndex,
+                        onClick = { currentIndex = index }
+                    )
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* Thêm mới */ },
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Thêm")
+
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        ) { it ->
+        Content(index = currentIndex, paddingValues = it)
     }
 }
+
+@Composable
+fun Content(index:Int, paddingValues: PaddingValues) {
+    when (index) {
+        0 -> NewsfeedScreen(paddingValues = paddingValues)
+        1 -> SearchScreen(paddingValues = paddingValues)
+        2 -> NotificationScreen(paddingValues = paddingValues)
+        3 -> ProfileScreen(paddingValues = paddingValues)
+    }
+}
+
+data class BottomNavItem(
+    val title: String,
+    val icon: ImageVector
+)
--- app/src/main/java/com/example/socialnetworkapp/view/NotificationScreen.kt
+++ app/src/main/java/com/example/socialnetworkapp/view/NotificationScreen.kt
@@ -1,2 +1,24 @@
 package com.example.socialnetworkapp.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NotificationScreen(modifier: Modifier = Modifier, paddingValues: PaddingValues) {
+    Column(modifier = Modifier.padding(paddingValues).fillMaxSize()) {
+        Text("Tiêu đề", style = MaterialTheme.typography.headlineSmall)
+        Text(
+            "Đây là nội dung bên trong NotificationScreen.",
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
