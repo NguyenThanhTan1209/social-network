e2295b7791b54ce521e8a8a1c820b24d75a822e7
nguyenthanhtan12981 <nguyenthanhtan1298@gmail.com>
2025-05-29 16:51:23 +0700

    vendor: Add firebase storage dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation(libs.firebase.bom)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
     //Hilt
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.navigation.compose)
